9163b1fe9c0665d0a48f72910c98e831f89b1f85
Christopher (Cody) Querubin <cody.querubin130@gmail.com>
2024-03-06 13:39:15 -0500

    Edit to map

--- live_weather_app/static/index.js
+++ live_weather_app/static/index.js
@@ -2,7 +2,7 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   const map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 38, lng: -77 },
     zoom: 8,
   });
 }
