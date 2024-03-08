4ae2d98ab3aeb71a6ba3b06339d4295bc0f50a1f
Christopher (Cody) Querubin <cody.querubin130@gmail.com>
2024-03-08 01:02:16 -0500

    Added Bootstrap / Put Data From OpenWeather API To Webpage

--- live_weather_app/views.py
+++ live_weather_app/views.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 def map(request):
     API_KEY = 'cdac524628de1773c07153a946813a62'
-    city_name = 'Paris'
+    city_name = 'Fairfax'
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric'
     response = requests.get(url).json()
     current_time = datetime.now()
@@ -20,10 +20,10 @@ def map(request):
         'city': city_name,
         'description': response['weather'][0]['description'],
         'icon': response['weather'][0]['icon'],
-        'temperature': 'Temperature: ' + str(response['main']['temp']) + ' °C',
+        'temperature': str(response['main']['temp']) + ' °C',
         'country_code': response['sys']['country'],
-        'wind': 'Wind: ' + str(response['wind']['speed']) + 'km/h',
-        'humidity': 'Humidity: ' + str(response['main']['humidity']) + '%',
+        'wind': str(response['wind']['speed']) + 'km/h',
+        'humidity': str(response['main']['humidity']) + '%',
          'time': formatted_time
     }
     context = {'city_weather_update': city_weather_update}
