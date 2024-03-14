4683598c26759df1712cd5a8b606a36f17ee16d8
sgarg26 <siddharth261202@gmail.com>
2024-03-14 15:29:17 -0400

    places added

--- live_weather_app/views.py
+++ live_weather_app/views.py
@@ -34,35 +34,7 @@ def get_geolocation(): # gets user ip and finds location.
 def get_meters(miles): # literally just converts miles to meters. 
     return round(miles*1609.34)
 
-def get_places(lat, lon, category, max_dist):
-    limit = 10
-    apiKey = "09008c734555433dbf212c6c61ebea3b"
-    max_dist = get_meters(max_dist)
-    url = f"https://api.geoapify.com/v2/places?categories={category}&filter=circle:{lon},{lat},{max_dist}&bias=proximity:{lon},{lat}&limit={limit}&apiKey={apiKey}"
-    resp = requests.get(url)
-    return resp.json()
-    
 
-def travel_advisor(request):
-    i = 0
-    list_places = []
-    if request.method == "POST":
-        form = travel_questionnaire(request.POST)
-        if form.is_valid():
-            loc = get_geolocation()
-            print("Location: ", loc)
-            max_dist = form.cleaned_data["max_dist"]
-            category = form.cleaned_data["category"]
-            resp = get_places(loc[0], loc[1], category, int(max_dist))
-            feat = resp["features"]
-            for i in range(len(feat)): # responses are not normalized, for now, just ignore if not returned in expected manner.
-                try:
-                    list_places.append(feat[i]["properties"]["name"])
-                except KeyError:
-                    continue
-            print(list_places)
-        # return render(request, "home/traveladvisor.html", {"list_places": list_places})
-    return render(request, "home/traveladvisor.html", {"form": travel_questionnaire(), "list_places": list_places})
 
 def get_weather_info(city):
     API_KEY = 'cdac524628de1773c07153a946813a62'
@@ -84,15 +56,71 @@ def get_weather_info(city):
     }
 
 def map(request):
-    get_weather_info('Fairfax')
+    if not request.method == "POST":
+        get_weather_info('Fairfax')
 
-    if request.method == "POST":
+    # if request.method == "POST":
+    else:
         form = NewCity(request.POST)
         if form.is_valid():
             cityInput = form.cleaned_data["cityInput"]
             get_weather_info(cityInput)
     context = {
         "city_weather_update": city_weather_update,
-        "place" : Place.objects.all()}
+        "place" : Place.objects.all(),
+        "travel_adivsor": None
+    }
     print(context)
+    travel_advisor()
     return render(request, 'home/index.html', context)
+
+def is_nice(temp):
+    return temp > 20
+
+def get_place_id(city):
+    url = f"https://api.geoapify.com/v1/geocode/search?text={city}&format=json&apiKey=09008c734555433dbf212c6c61ebea3b"
+    resp = requests.get(url)
+    return resp.json()["results"][0]["place_id"]
+
+def travel_advisor():
+    list_places = []
+    # loc = get_geolocation()
+
+    # curr_temp = float(city_weather_update["temperature"].split()[0])
+    curr_city = city_weather_update["city"]
+    city_id = get_place_id(curr_city)
+    print(city_id)
+    resp = get_places(city_id)
+    for i in resp["features"]:
+        list_places.append(i["properties"]["name"])
+    print(list_places)
+    # if is_nice(curr_temp):
+
+def get_places(place_id):
+    apiKey = "09008c734555433dbf212c6c61ebea3b"
+    # max_dist = get_meters(max_dist)
+    url = f"https://api.geoapify.com/v2/places?categories=tourism&filter=place:{place_id}&limit=5&apiKey={apiKey}"
+    resp = requests.get(url)
+    return resp.json()
+
+'''def travel_advisor2(request):
+    i = 0
+    list_places = []
+    if request.method == "POST":
+        form = travel_questionnaire(request.POST)
+        if form.is_valid():
+            loc = get_geolocation()
+            print("Location: ", loc)
+            max_dist = form.cleaned_data["max_dist"]
+            category = form.cleaned_data["category"]
+            resp = get_places(loc[0], loc[1], category, int(max_dist))
+            feat = resp["features"]
+            for i in range(len(feat)): # responses are not normalized, for now, just ignore if not returned in expected manner.
+                try:
+                    list_places.append(feat[i]["properties"]["name"])
+                except KeyError:
+                    continue
+            print(list_places)
+        # return render(request, "home/traveladvisor.html", {"list_places": list_places})
+    # return render(request, "home/traveladvisor.html", {"form": travel_questionnaire(), "list_places": list_places})
+    '''
