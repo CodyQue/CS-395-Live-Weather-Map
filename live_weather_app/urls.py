af4be4350e73728571a0d82c1621e5089def9cbc
Christopher (Cody) Querubin <cody.querubin130@gmail.com>
2024-03-21 12:47:16 -0400

    Added Humidity Map

--- live_weather_app/urls.py
+++ live_weather_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.map, name="map"),
-    path('traveladvisor', views.travel_advisor, name="travel"),
-    path('windmap',views.windmap, name = 'windmap' )
+    path('windmap',views.windmap, name = 'windmap' ),
+    path('humiditymap', views.humiditymap, name = 'humiditymap')
 ]
